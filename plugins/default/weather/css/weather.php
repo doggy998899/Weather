bf3348c096578178e2778ff1fe24b31a88ca9114
doggy998899 <31792001+doggy998899@users.noreply.github.com>
2017-09-14 19:12:23 -0400

    Add files via upload

---
+++ plugins/default/weather/css/weather.php
@@ -0,0 +1,18 @@
+.menu-section-item-weather:before {
+    content: "\f185" !important;
+}
+
+.weather-container {
+    position: relative;
+    padding-bottom: 75%;
+    height: 0;
+    overflow: hidden;
+}
+
+.weather-container iframe {
+    position: absolute;
+    top:0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
---
+++ plugins/default/weather/weather.php
@@ -0,0 +1,15 @@
+<head>
+ <link rel="stylesheet" href="css/weather"> 
+ <meta charset = "UTF-8">
+ <title></title>
+ <link rel = "stylesheet"
+   type = "text/css"
+   href = "css/weather" />
+ </head>
+ <body>
+<div id="containingBlock">
+ <div class="weather-container">
+    <iframe src="//cdnres.willyweather.com/widget/loadView.html?id=71511style=" border-width:0 " width="700" height="520" frameborder="0" scrolling="no">
+    </iframe>
+</div>
+ </body>
